a17c4a7715088806dddec888580bdfe7db047a7c
Tim Cook <cooktt314@gmail.com>
2024-01-06 20:44:06 -0500

    Add French3 with writing audio files.

---
+++ FrenchPythonDev/StartAudioExcel - Copy.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+:: Set the path to your virtual environment's activate.bat
+set env_activate=.\env\Scripts\activate.bat
+
+:: Activate the virtual environment
+call "%env_activate%"
+
+:: Check if the environment activation was successful
+if %errorlevel% neq 0 (
+    echo Error: Failed to activate the virtual environment.
+    pause
+    exit /b
+)
+
+@rem echo path
+@rem pause
+
+@rem start cmd.exe /k
+
+@rem start cmd.exe
+
+
+:: Launch Excel with the specified workbook
+start cmd.exe
---
+++ FrenchPythonDev/tryDuo.py
@@ -0,0 +1,11 @@
+
+
+
+import duolingo
+
+import duolingo
+#lingo  = duolingo.Duolingo('TimCook687383')
+# or
+lingo  = duolingo.Duolingo('TimCook687383', password='pieberry')
+
+print(lingo.get_user_info())
